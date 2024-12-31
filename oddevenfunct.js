19c3741599f33879b486c626ab3d51b2edd91c80
Mohsin678 <mohsin21.kh@gmail.com>
2024-12-31 17:27:49 +0530

    program using function to find even odd

---
+++ oddevenfunct.js
@@ -0,0 +1,11 @@
+function oddeven(n){
+    let remainder = n %2;
+    if(remainder ==0){
+        console.log("the number is even");
+    }
+    else{
+        console.log("the number is odd");
+
+    }
+}
+console.log(oddeven(3));
